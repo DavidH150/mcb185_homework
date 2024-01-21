66a2683ddf24ca805cd35bb456e9da2a80304a2e
DavidH150 <drhurd@ucdavis.edu>
2024-01-21 11:12:58 -0800

    hopefully this works?

---
+++ 13spellingbee.sh
@@ -0,0 +1,10 @@
+gunzip -c ../MCB185/data/dictionary.gz | grep "r" | grep -E "[airconz]{4,}" | grep -v "[^airconz]" 
+gunzip -c ../MCB185/data/dictionary.gz | grep "r" | grep -E "[airconz]{4,}" | grep -v "[^airconz]" | wc
+
+
+
+
+
+
+
+
