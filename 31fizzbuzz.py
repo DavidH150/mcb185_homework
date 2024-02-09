b2b81c7f047ee6504d88783368d4da82d0dc2f34
DavidH150 <drhurd@ucdavis.edu>
2024-02-08 16:16:51 -0800

    fixed

--- 31fizzbuzz.py
+++ 31fizzbuzz.py
@@ -1,4 +1,6 @@
 for i in range(1, 100):
-	if i % 3 == 0: print(i, 'Fizz')
-	if i % 5 == 0: print(i, 'Buzz')
-	if i % 3 == 0 and i % 5 == 0: print(i, 'FizzBuzz')
+	output = i
+	if i % 3 == 0: output = 'Fizz'
+	if i % 5 == 0: output = 'Buzz'
+	if i % 3 == 0 and i % 5 == 0: output = 'FizzBuzz'
+	print(output)
